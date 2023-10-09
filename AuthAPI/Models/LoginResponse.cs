ec382d74330358b7d0f2a58a6df6696f999085e9
unknown <hoangtung4398@gmail.com>
2023-10-09 17:35:04 +0700

    10/9/2023

--- AuthAPI/Models/LoginResponse.cs
+++ AuthAPI/Models/LoginResponse.cs
@@ -2,7 +2,7 @@
 {
     public class LoginResponse
     {
-        public UserInfo User { get; set; }
+        public UserDTO User { get; set; }
         public string Token { get; set; }
     }
 }
---
+++ Mango/Service/IService/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web;
+
+public interface IAuthService
+{
+    Task<ResponseDto> LoginAsync(L model);
+    
+}
