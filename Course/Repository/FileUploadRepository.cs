9dcb784b47cca520cec5eb6444ade135d65ba22e
tung4398 <hoangtung4398@gmail.com>
2024-01-10 00:04:52 +0700

    commit

---
+++ Course/Repository/FileUploadRepository.cs
@@ -0,0 +1,16 @@
+﻿using CouponAPI.Repository;
+using CourseAPI.Models;
+using CourseAPI.Repository.IRepository;
+
+namespace CourseAPI.Repository
+{
+    public class FileUploadRepository : Repository<FileUpload>, IFileUploadRepository
+    {
+        private readonly MangoAuthContext _db;
+
+        public FileUploadRepository(MangoAuthContext dbcontext) : base(dbcontext)
+        {
+        }
+
+    }
+}
