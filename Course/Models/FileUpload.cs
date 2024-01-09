7d5e9f8bd2f10359e36a413e94a3d67aeedb79b6
tung4398 <hoangtung4398@gmail.com>
2024-01-09 23:49:17 +0700

    commit

---
+++ Course/Models/FileUpload.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CourseAPI.Models;
+
+public partial class FileUpload : BaseEntity
+{
+
+    public string? NameAz { get; set; }
+
+    public string? NameClient { get; set; }
+
+    public string? Url { get; set; }
+}
