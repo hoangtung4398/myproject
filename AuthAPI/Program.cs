fa587fe78ca908a687a4377a187613d2b06e37be
hoangtung4398 <hoangtung4398@gmail.com>
2024-01-26 09:56:10 +0700

    commit

--- AuthAPI/Program.cs
+++ AuthAPI/Program.cs
@@ -29,11 +29,9 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
