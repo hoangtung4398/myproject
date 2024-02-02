3e463f704c3dd87c550531a771bff2c73cdfb3d9
hoangtung4398 <hoangtung4398@gmail.com>
2024-02-02 18:06:51 +0700

    commit

---
+++ UserCourse/Program.cs
@@ -0,0 +1,42 @@
+using CourseAPI.Repository.IRepository;
+using CourseAPI.Repository;
+using CourseAPI.Services.IService;
+using CourseAPI.Services;
+using BaseCourse.Models;
+using Microsoft.EntityFrameworkCore;
+using AuthAPI.Repositorys.IRepository;
+using AuthAPI.Repositorys;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<CourseContext>(option =>
+{
+	option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddScoped<ICourseRepository, CourseRepository>();
+builder.Services.AddScoped<IGetUserService, GetUserService>();
+builder.Services.AddScoped<IUserCourseRepository, UserCourseRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
