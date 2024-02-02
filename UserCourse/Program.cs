527d9f348ad6577cb38a139cd0bf0c4455a236a3
tung4398 <hoangtung4398@gmail.com>
2024-02-02 19:02:42 +0700

    commit

--- UserCourse/Program.cs
+++ UserCourse/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using AuthAPI.Repositorys.IRepository;
 using AuthAPI.Repositorys;
 using UserCourseAPI.Middleware;
+using CourseAPI.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +20,7 @@ builder.Services.AddScoped<ICourseRepository, CourseRepository>();
 builder.Services.AddScoped<IGetUserService, GetUserService>();
 builder.Services.AddScoped<IUserCourseRepository, UserCourseRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddTransient<TokenMiddleware>();
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -28,11 +30,9 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
 app.UseSwagger();
 app.UseSwaggerUI();
-app.UseMiddleware<GetTokenMiddleware2>();
-
+app.UseMiddleware<TokenMiddleware>();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
